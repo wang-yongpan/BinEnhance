26d459364292573d66f2ab2a442f2c1065b82034
Wang Yongpan <43602467+wang-yongpan@users.noreply.github.com>
2024-09-27 21:58:11 +0800

    Update settings.py

--- IDA_scripts/settings.py
+++ IDA_scripts/settings.py
@@ -1,1 +1,13 @@
+# coding:utf-8
+
+# the path of python3 library
+PACKAGE_PATH = "xxx/lib/python3.8/site-packages"
+
+# the path of IDA Pro
+IDA_PATH = "xxx/idapro-7.5/idat64"
+
+IDA_VERSION = 7.5
+
+# the path of binary files
+DATA_BASE = "xxx"
 
